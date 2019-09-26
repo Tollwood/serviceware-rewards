d5ebef7906d7f3ee86091cd6ee65f0880b769ade
Loic Pennec <loic.pennec@serviceware.se>
2019-09-26 14:47:04 +0200

    Stats View Progress

--- client/src/app/stats/stats.component.ts
+++ client/src/app/stats/stats.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../models/user.model';
+import { StatsService } from '../services/stats.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-stats',
@@ -7,19 +9,39 @@ import { User } from '../models/user.model';
   styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements OnInit {
-  users : User[] = [{
-    name : 'Loic',
-    title : 'Master',
-    currentXp : 1,
-    previousLevelXp : 2,
-    nextLevelXp : 3,
-  }];
+  userId : number = 123;
+  currentUser : Observable<User>;
+  tag : string = 'XP';
+  progressBarPercentage : string;
 
-  curretUser : User = this.users[0];
+  tempUser : User = new User(
+    {
+      name : 'Loic Pennec',
+      rank : 'Dungeon Master',
+      xp : 125,
+      xpNext : 200,
+      xpPrev : 100,
+      pictureUrl : 'https://s.cdpn.io/profiles/user/777898/512.jpg?1'
+    }
+  );
 
-  constructor() { }
+  constructor(private statsService : StatsService) {  
+      //this.currentUser = statsService.getStats(this.userId); 
+      this.currentUser = of(this.tempUser);
+      this.progressBarPercentage = (this.tempUser.xp - this.tempUser.xpPrev) * 100 / (this.tempUser.xpNext - this.tempUser.xpPrev) + '%';
+      console.log(this.tempUser.xp);
+      console.log(this.tempUser.xpNext);
+      console.log(this.tempUser.xpPrev);
+      console.log(this.progressBarPercentage);
+  }
 
   ngOnInit() {
+    /*this.statsService.getStats(this.userId).subscribe(
+      (data) => {
+        this.currentUser = of(data);
+        this.progressBarPercentage = (this.tempUser.xp - this.tempUser.xpPrev) * 100 / (this.tempUser.xpNext - this.tempUser.xpPrev) + '%';
+      }
+    );*/
   }
 
 }
