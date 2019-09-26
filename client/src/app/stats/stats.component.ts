d66d087ad526fc783cf3b4d4586214c57997624d
Loic Pennec <loic.pennec@serviceware.se>
2019-09-26 12:31:19 +0200

    Stats Update with Users

--- client/src/app/stats/stats.component.ts
+++ client/src/app/stats/stats.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.sass']
+  styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements OnInit {
+  users : User[] = [{
+    name : 'Loic',
+    title : 'Master',
+    currentXp : 1,
+    previousLevelXp : 2,
+    nextLevelXp : 3,
+  }];
+
+  curretUser : User = this.users[0];
 
   constructor() { }
 
