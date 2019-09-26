1f063c7d7768d18aba1c16a0cf9aa7f70b1d91a3
Loic Pennec <loic.pennec@serviceware.se>
2019-09-26 15:59:23 +0200

    Statts

---
+++ client/src/app/stats/sparkle/sparkle.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sparkle',
+  templateUrl: './sparkle.component.html',
+  styleUrls: ['./sparkle.component.scss']
+})
+export class SparkleComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
