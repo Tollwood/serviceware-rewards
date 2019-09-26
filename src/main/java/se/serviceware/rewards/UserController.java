0a44b110f15be76d248746a50001f35acece8fb3
jthielscher <jens.thielscher@sabio.de>
2019-09-26 12:17:43 +0200

    replaced local user storage by in memory one

--- src/main/java/se/serviceware/rewards/UserController.java
+++ src/main/java/se/serviceware/rewards/UserController.java
@@ -3,6 +3,7 @@ package se.serviceware.rewards;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @RequestMapping(path = "users", method = RequestMethod.GET)
-    public List<User> users() {
-        return Arrays.asList(new User(0, 10, 0, "Noob", ""));
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        userRepository.save(new User("Jens", 0, 10, 0, "Noob", ""));
+        userRepository.save(new User("Theo", 0, 10, 0, "Noob", ""));
+        userRepository.save(new User("Tobi", 0, 10, 0, "Noob", ""));
     }
 
+    @RequestMapping(path = "users", method = RequestMethod.GET)
+    public List<User> users() {
+        return userRepository.findAll();
+    }
+
+    @RequestMapping(path = "users/{username}", method = RequestMethod.GET)
+    public User user(@PathVariable String username) {
+        return userRepository.findById(username).get();
+    }
 }
